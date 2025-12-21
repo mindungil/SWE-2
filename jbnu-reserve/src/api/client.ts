7795be53e97385f1881c3ecdb65e5350ac257961
Quinsie <jyjh0501@naver.com>
2025-12-21 21:41:05 +0900

    fix: modified client.ts (wrong port number)

--- jbnu-reserve/src/api/client.ts
+++ jbnu-reserve/src/api/client.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://localhost:8000";
 
 export const apiFetch = async (endpoint: string, options: RequestInit = {}) => {
   const token = localStorage.getItem("access_token");
